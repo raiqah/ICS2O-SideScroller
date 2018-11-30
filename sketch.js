472f38dbe0bf362c68ce55cf7d4c449d6765926f
raiqah <42979864+raiqah@users.noreply.github.com>
2018-11-30 13:01:20 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,41 +5,15 @@
 // Session 2: Array of Particles, multiple forces
 
 var particles = [];
-var person; 
+var man; 
 
 function setup() {
   createCanvas(640, 360);
+	man = new Person(); 
 }
 
-function mousePressed() {
-  var p = new Particle(mouseX, mouseY, random(2,4));
-  particles.push(p);
-}
-
-function keyPressed() {
-  if (key == ' ') {
-    particles.splice(0, 1);
-  }
-
-}
-
-function draw() {
-  background(51);
-	person[i].update();
-	person[i].display();
-  var wind = createVector(0.1, 0);
-
-  for (var i = 0; i < particles.length; i++) {
-    var gravity = createVector(0, 0.2 * particles[i].mass);
-    particles[i].applyForce(gravity);
-
-    // Wind is applied only if mouse is pressed
-    if (mouseIsPressed) {
-      particles[i].applyForce(wind);
-    }
-
-    particles[i].update();
-    particles[i].edges();
-    particles[i].display();
-  }
+function draw (){
+background(51);
+	man.update();
+	man.display();
 }
