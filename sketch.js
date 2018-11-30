0451149d3e281cc65598d67c2b780073c7fd4f9b
raiqah <42979864+raiqah@users.noreply.github.com>
2018-11-30 13:46:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
-// Daniel Shiffman
-// Daniel Shiffman
-// https://www.kadenze.com/courses/the-nature-of-code
-// http://natureofcode.com/
-// Session 2: Array of Particles, multiple forces
-
-var particles = [];
 var man; 
 
 function setup() {
-  createCanvas(640, 360);
+  createCanvas(400, 400);
 	man = new Person(); 
 }
 
+var x;
+x= -100;
+
 function draw (){
 background(51);
+
+	translate(-man.pos.x+50,0);
 	man.update();
 	man.display();
+	
+	fill(12,239,23);
+	rect(250,350,100,100);
 }
